13030bf8ea565186721928193bd1660c2319a36d
stephen22otieno <stephenombiro22@gmail.com>
2018-10-31 14:40:39 +0300

    src/app/quotes/quotes.component.ts

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -7,25 +7,25 @@ import {Quotes} from '../quotes';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
-  Quotes=[
-    new Quotes ('If you see something, say something!','Katana',0,0,),
-    new Quotes ('No coffee, no coding!','Cliff',0,0), 
-    new Quotes ('If you wonna copy, be wise','Steve',0,0),
-  ]
+  Quotes = [
+    new Quotes ('If you see something, say something!', 'Katana', 0, 0, ),
+    new Quotes ('No coffee, no coding!', 'Cliff', 0, 0),
+    new Quotes ('If you wonna copy, be wise!', 'Steve', 0, 0),
+  ];
   constructor() { }
 
   ngOnInit() {
   }
-  like(i){
-    this.Quotes[i].likes+=1
+  like(i) {
+    this.Quotes[i].likes += 1;
   }
-  dislike(i){
-    this.Quotes[i].dislikes+=1
+  dislike(i) {
+    this.Quotes[i].dislikes += 1;
   }
-  Delete(i){
-    this.Quotes.splice(i,1)
+  Delete(i) {
+    this.Quotes.splice(i, 1);
   }
-new(quote){
+new(quote) {
   this.Quotes.push(quote);
 }
 }
